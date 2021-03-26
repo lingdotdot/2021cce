2e849170cbd1acfca6a18dd0bb6a8ff878e3bf8f
lingdotdot <79676815+lingdotdot@users.noreply.github.com>
2021-03-26 09:59:43 +0800

    Add files via upload

---
+++ week05-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char line[10]="decline";
+    char line2[10]={'p','r','o','p','e','r','\o'};
+
+    printf("%s\n", line);
+    printf("%s\n", line2);
+
+    char line3[]={'m', 'a','j','o','r','i','t','y'};
+    printf("%s\n", line3);
+}
