918d0838f0b52473674eef717de892e5fa7861f1
lingdotdot <79676815+lingdotdot@users.noreply.github.com>
2021-04-30 11:27:56 +0800

    Add files via upload

---
+++ week10/step 4.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char line[1000];
+char tree[1000000][32];
+int compare( const void *p1, const void *p2){
+	return strcmp ( (char*) p1, (char*) p2);
+}
+int main()
+{
+	int T;
+	scanf("%d\n\n", &T);
+
+	for(int t=0; t<T; t++){
+		int N=0;
+		while(gets(line)!=NULL ){
+			if(strcmp(line,"")==0)break;
+
+			strcpy( tree[N], line);
+
+			N++;
+		}
+		printf("¦³´X´Ê¾ð? %d\n", N);
+
+		qsort( tree, N, 32, compare);
+
+		for(int i=0; i<N; i++){
+			printf("%s\n", tree[i]);
+		}
+		printf("=======\n");
+	}
+}
